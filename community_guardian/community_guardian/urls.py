f57dfbb4bb5aba1f065fbd020c1d65645b205fcc
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-13 15:05:54 +0530

    add hot reloading

--- community_guardian/community_guardian/urls.py
+++ community_guardian/community_guardian/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.leanding, name='leanding')
+    path('', views.leanding, name='leanding'),
+
+    #Hot reloading
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
