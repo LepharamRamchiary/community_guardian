a83fc220224a82c8ec8eb569ca9d7b6ffc3ecf3d
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-14 15:01:19 +0530

    register issue model

--- community_guardian/issue/admin.py
+++ community_guardian/issue/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Issue
 
-# Register your models here.
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'status', 'created_at')
+    list_filter = ('status', 'category')
+    search_fields = ('title', 'description')
