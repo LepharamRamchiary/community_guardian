0c481a31d57b1415a000100304a16f1b9d3f10be
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-14 14:58:12 +0530

    add model for reporting issue

--- community_guardian/issue/models.py
+++ community_guardian/issue/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Issue(models.Model):
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Processing', 'Processing'),
+        ('Resolved', 'Resolved'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='issue_images/')
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
