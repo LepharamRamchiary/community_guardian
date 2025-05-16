aa03f2b75694c24611e0c25cb39141f0b77141ef
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-16 13:27:05 +0530

    add user are editable function

--- community_guardian/issue/forms.py
+++ community_guardian/issue/forms.py
@@ -5,3 +5,9 @@ class IssueForm(forms.ModelForm):
     class Meta:
         model = Issue
         fields = ['title', 'category', 'image', 'description', 'location']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+        }
