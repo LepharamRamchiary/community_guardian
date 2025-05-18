bfc3dfd613b8ebb720135de51b9b0d4eb3dd6cd1
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-18 21:52:36 +0530

    .

--- community_guardian/community_guardian/settings.py
+++ community_guardian/community_guardian/settings.py
@@ -151,3 +151,23 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
