6c6e4d9b8d853e85655fd8072d2cf0b5d9436222
LepharamRamchiary <lepharamchiary@gmail.com>
2025-05-19 11:42:42 +0530

    create admin

--- community_guardian/build.sh
+++ community_guardian/build.sh
@@ -17,4 +17,4 @@ python manage.py migrate
 
 # Create a superuser (optional)
 # If you want to create a superuser during deployment, uncomment the next line
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'admin123')" | python manage.py shell
